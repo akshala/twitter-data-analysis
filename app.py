2e34a05d102e86cff8b8c88d9b0606aab62bb3c4
Akshala Bhatnagar <47988358+akshala@users.noreply.github.com>
2021-01-01 16:12:33 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask import request, url_for
+from flask import request, current_app
+from flask_restful import Resource, Api
+
+app = Flask(__name__, static_folder='./static', template_folder='./templates')
+
+@app.route('/', methods = ['POST', 'GET', 'OPTIONS'])
+def tensorboard():
+ 	return render_template('tensorboard.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
